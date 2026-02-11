a95c6414ab4a57e9bd455efcd6cfc5fc7d0abc4f
WSR-2 <iwsr2@mail.ru>
2026-02-11 15:27:12 +0800

    - add new activities

--- app/src/main/java/com/example/olimpiada/MainActivity.kt
+++ app/src/main/java/com/example/olimpiada/MainActivity.kt
@@ -1,20 +1,48 @@
 package com.example.olimpiada
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.Refresh
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.LinkAnnotation
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.TextLinkStyles
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
+import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.text.withLink
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.example.olimpiada.presentation.CreateProfile
 import com.example.olimpiada.ui.theme.OlimpiadaTheme
 import com.example.uikit.components.BigPrimaryBtn
 import com.example.uikit.components.TestBigPrimaryBtn
@@ -34,10 +62,129 @@ class MainActivity : ComponentActivity() {
                             .padding(innerPadding)
                             .fillMaxSize()
                     ) {
-                        InputName()
+                        SignIn(
+                            modifier = Modifier.padding(innerPadding)
+                        )
+                        AnySignIn(
+                            modifier = Modifier.padding(innerPadding)
+                        )
                     }
                 }
             }
+        }
+    }
+}
+
+@Composable
+fun SignIn(modifier: Modifier = Modifier) {
+    Column(
+        modifier = modifier
+            .padding(32.dp)
+    ) {
+        Text(
+            text = "Добро пожаловать\n"
+        )
+        Text(
+            text = "Войдите, чтобы пользоваться функциями приложения \n"
+        )
+
+        var text by rememberSaveable { mutableStateOf("") }
+
+        TextField(
+            value = text,
+            onValueChange = { text = it },
+            label = { Text("Вход по E-mail") },
+            placeholder = { Text("example@gmail.com") }
+        )
+
+        var password by rememberSaveable { mutableStateOf("") }
+        var passwordHidden by rememberSaveable { mutableStateOf(true) }
+        TextField(
+            value = password,
+            onValueChange = { password = it },
+            singleLine = true,
+            label = { Text("Пароль") },
+            visualTransformation =
+                if (passwordHidden) PasswordVisualTransformation() else VisualTransformation.None,
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
+            trailingIcon = {
+                IconButton(onClick = { passwordHidden = !passwordHidden }) {
+                    val visibilityIcon =
+                        if (passwordHidden) Icons.Filled.Refresh else Icons.Filled.Refresh
+                    // Please provide localized description for accessibility services
+                    val description = if (passwordHidden) "Показать пароль" else "Скрыть пароль"
+                    Icon(imageVector = visibilityIcon, contentDescription = description)
+                }
+            }
+        )
+
+        Button(
+            onClick = {
+            },
+            modifier = Modifier
+                .align(Alignment.CenterHorizontally)
+        ) {
+            Text("Далее")
+        }
+
+        val url = ""
+
+        val linkColor = MaterialTheme.colorScheme.primary
+        val linkStyle = SpanStyle(color = linkColor, textDecoration = TextDecoration.Underline)
+
+        val annotatedString = buildAnnotatedString {
+            withLink(LinkAnnotation.Url(url = url, styles = TextLinkStyles(style = linkStyle))) {
+                append("Зарегистрироваться")
+            }
+        }
+        Text(
+            modifier = Modifier
+                .align(Alignment.CenterHorizontally),
+            text = annotatedString
+        )
+
+    }
+}
+
+@Composable
+fun AnySignIn(modifier: Modifier = Modifier) {
+    Column(
+        modifier = Modifier
+            .padding(32.dp),
+    ) {
+        Text(
+            modifier = Modifier,
+            text = "Или войдите с помощью",
+        )
+
+        Button(
+            modifier = Modifier
+                .align(Alignment.CenterHorizontally),
+            onClick = { /* Do something! */ },
+            contentPadding = ButtonDefaults.ButtonWithIconContentPadding
+        ) {
+            Icon(
+                Icons.Filled.Favorite,
+                contentDescription = "Localized description",
+                modifier = Modifier.size(ButtonDefaults.IconSize)
+            )
+            Spacer(Modifier.size(ButtonDefaults.IconSpacing))
+            Text("Войти с VK")
+        }
+
+        Button(
+            modifier = Modifier
+                .align(Alignment.CenterHorizontally),
+            onClick = { /* Do something! */ },
+            contentPadding = ButtonDefaults.ButtonWithIconContentPadding
+        ) {
+            Icon(
+                Icons.Filled.Favorite,
+                contentDescription = "Localized description",
+                modifier = Modifier.size(ButtonDefaults.IconSize)
+            )
+            Spacer(Modifier.size(ButtonDefaults.IconSpacing))
+            Text("Войти с Yandex")
         }
     }
 }
@@ -52,7 +199,9 @@ fun TestAll() {
                     .padding(innerPadding)
                     .fillMaxSize()
             ) {
-                InputName()
+                SignIn()
+                AnySignIn()
+
             }
         }
     }
